74153cae187d8fff590fff9e89887da52a075f81
kuranovicka <kuranovicka@gmail.com>
2026-07-23 19:54:14 +0200

    Add Copy text and Share as .txt options

--- app/src/main/java/org/jak_linux/dns66/HelpActivity.java
+++ app/src/main/java/org/jak_linux/dns66/HelpActivity.java
@@ -7,9 +7,24 @@
  */
 package org.jak_linux.dns66;
 
+import android.content.ClipData;
+import android.content.ClipboardManager;
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
 
 public class HelpActivity extends AppCompatActivity {
 
@@ -23,6 +38,59 @@ public class HelpActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.setTitle(R.string.action_help);
             actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.add(0, 1, 0, R.string.action_copy_text);
+        menu.add(0, 2, 1, R.string.action_share);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == android.R.id.home) {
+            finish();
+            return true;
+        } else if (item.getItemId() == 1) {
+            copyHelpText();
+            return true;
+        } else if (item.getItemId() == 2) {
+            shareHelpTextAsFile();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void copyHelpText() {
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("ZbogomReklame - Pomoć", getString(R.string.help_text));
+        if (clipboard != null) {
+            clipboard.setPrimaryClip(clip);
+            Toast.makeText(this, R.string.copied_to_clipboard, Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void shareHelpTextAsFile() {
+        try {
+            File sharedDir = new File(getCacheDir(), "shared");
+            if (!sharedDir.exists())
+                sharedDir.mkdirs();
+            File file = new File(sharedDir, "ZbogomReklame-pomoc.txt");
+            OutputStreamWriter writer = new OutputStreamWriter(new FileOutputStream(file), "UTF-8");
+            writer.write(getString(R.string.help_text));
+            writer.close();
+
+            Uri uri = FileProvider.getUriForFile(this, "org.jak_linux.dns66.fileprovider", file);
+
+            Intent shareIntent = new Intent(Intent.ACTION_SEND);
+            shareIntent.setType("text/plain");
+            shareIntent.putExtra(Intent.EXTRA_STREAM, uri);
+            shareIntent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+            startActivity(Intent.createChooser(shareIntent, getString(R.string.action_share)));
+        } catch (IOException e) {
+            Toast.makeText(this, R.string.generic_error, Toast.LENGTH_SHORT).show();
         }
     }
 }
